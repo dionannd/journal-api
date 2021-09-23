22fb82ebeeb54e44b74838d05b4e529f8973eccc
dionannd <dionananda77@gmail.com>
2021-09-24 01:21:34 +0700

    add Transaction Detail Controller

--- src/controller/transaction/TransactionController.js
+++ src/controller/transaction/TransactionController.js
@@ -1,71 +1,75 @@
 import { mainDb } from "../../lib/db";
 
 class TransactionController {
-    constructor() {
-        this.db = mainDb
-    }
+  constructor() {
+    this.db = mainDb;
+  }
 
-    transaction = async (req, res) => {
-        try {
-             const data = await this.db.query(
-                `select * from transaction_headers order by transaction_header_id desc`
-            )
-            const result = data.map((transaction) => {
-                return {
-                ...transaction,
-                };
-            });
-            res.status(200).send({data:result })
-        } catch (error) {
-            res.status(500).send({message: error.message})
-        }
-    }
-
-    insertTransaction = async (req, res) => {
-        try {
-            const {body} = req
-            body.user_id = req.user.id;
-            await this.db.query(
-                `insert into transaction_headers (name, user_id, created_at) values ($<name>, $<user_id>, to_timestamp(${Date.now()} / 1000.0))`, body
-            )
-            res.status(200).send({message: "Transaksi berhasil ditambahkan"})
-        } catch (error) {
-            res.status(500).send({message: error.message})
-        }
-    }
-
-    editTransaction = async (req, res) => {
-        try {
-            const {body} = req
-            body.transaction_header_id = req.params.id;
-            await this.db.query(
-                `update transaction_headers set name = $<name> where transaction_header_id = $<transaction_header_id>
-                returning *`, body
-            )
-            res.status(200).send({message: "Transaksi berhasil diperbaharui"})
-        } catch (error) {
-            res.status(500).send({message: error.message})
-        }
-    }
-
-    deleteTransaction = async (req, res) => {
-        try {
-        await this.db.query(
-            `delete from transaction_headers where transaction_header_id = $1`,
-            req.params.id
-        );
-        res.status(200).send({ message: "Transaksi berhasil dihapus" });
-        } catch (error) {
-        res.status(500).send({ message: error.message });
-        }
-    };
-    detailTransaction = async (req, res) => {
+  transaction = async (req, res) => {
     try {
-      const detailTransaction = await this.db.oneOrNone(
-        `select * from transaction_headers where transaction_header_id = $1`,
+      const data = await this.db.query(
+        `select th.*, (
+          select coalesce(sum(amount), 0) as total
+          from transaction_details td
+          where td.transaction_id = th.transaction_id
+        ) from transaction_headers th
+        where user_id = $1
+        group by th.transaction_id`,
+        req.user.id
+      );
+      return res.status(200).send({ data });
+    } catch (error) {
+      res.status(500).send({ message: error.message });
+    }
+  };
+
+  insertTransaction = async (req, res) => {
+    try {
+      const { body } = req;
+      body.user_id = req.user.id;
+      await this.db.query(
+        `insert into transaction_headers (name, description, user_id, created_at) values ($<name>, $<description>, $<user_id>, NOW())`,
+        body
+      );
+      res.status(200).send({ message: "Transaksi berhasil ditambahkan" });
+    } catch (error) {
+      res.status(500).send({ message: error.message });
+    }
+  };
+
+  editTransaction = async (req, res) => {
+    try {
+      const { body } = req;
+      body.transaction_header_id = req.params.id;
+      await this.db.query(
+        `update transaction_headers set name = $<name> where transaction_id = $<transaction_id>
+                returning *`,
+        body
+      );
+      res.status(200).send({ message: "Transaksi berhasil diperbaharui" });
+    } catch (error) {
+      res.status(500).send({ message: error.message });
+    }
+  };
+
+  deleteTransaction = async (req, res) => {
+    try {
+      await this.db.query(
+        `delete from transaction_headers where transaction_id = $1`,
         req.params.id
       );
-    
+      res.status(200).send({ message: "Transaksi berhasil dihapus" });
+    } catch (error) {
+      res.status(500).send({ message: error.message });
+    }
+  };
+  detailTransaction = async (req, res) => {
+    try {
+      const detailTransaction = await this.db.oneOrNone(
+        `select * from transaction_headers where transaction_id = $1`,
+        req.params.id
+      );
+
       res.status(200).send({ data: detailTransaction });
     } catch (error) {
       res.status(500).send({ message: error.message });
@@ -73,4 +77,4 @@ class TransactionController {
   };
 }
 
-export default TransactionController
+export default TransactionController;
--- src/migrations/system/20210915210833_transaction_headers.js
+++ src/migrations/system/20210915210833_transaction_headers.js
@@ -1,17 +1,15 @@
-
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.raw(
-      `create table if not exists transaction_headers(
-          transaction_header_id serial primary key not null,
+    `create table if not exists transaction_headers(
+          transaction_id serial primary key not null,
           name varchar,
+          description varchar,
           user_id integer,
           created_at timestamptz
       )`
-  )
+  );
 };
 
-exports.down = function(knex) {
-  return knex.schema.raw(
-      `drop table if exists transaction_headers`
-  )
+exports.down = function (knex) {
+  return knex.schema.raw(`drop table if exists transaction_headers`);
 };
